11baeb681aa8c9a2db5fb4abcc9e807d50a38766
ankush4gupta <87088148+ankush4gupta@users.noreply.github.com>
2022-02-25 20:50:39 +0530

    added product controller, model, DB connect file

--- app.js
+++ app.js
@@ -1,5 +1,42 @@
-<<<<<<< Updated upstream
-const express
-=======
-const 
->>>>>>> Stashed changes
+const express = require("express");
+
+const path = require("path");
+const connect = require("./configs/db");
+const app = express();
+const static_path = path.join(__dirname, "./public");
+const productcontroller = require("./controllers/product.controller");
+
+app.use(express.json());
+
+
+const port = process.env.PORT || 4493;
+app.use(express.static(static_path))
+
+//  index route 
+app.get("", (req, res) => {
+    try {
+        res.send("homepage")
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+
+
+
+
+// product route 
+app.use("/product", productcontroller)
+
+
+
+
+// connect function 
+app.listen(port, async () => {
+    try {
+        await connect()
+        console.log("listening port 4493")
+    } catch (error) {
+        console.log(error.message)
+    }
+})
