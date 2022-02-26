bf9a146919685f8c37468303ee20764743c535e2
Prashant-SL <pacchidon07@gmail.com>
2022-02-26 12:11:09 +0530

    updated index and footer
    updated index and footer

--- public/footerComponents/footer.js
+++ public/footerComponents/footer.js
@@ -1,91 +1,77 @@
 function footer() {
-  return ` <div id="footerDiv">
-    <div id="firstDivF">
-      <div id="footerBag">
-        <img
-          src="https://cdn-icons-png.flaticon.com/128/709/709488.png"
-          alt=""
-        />
-        <p>EASY EXCHANGE</p>
-      </div>
-      <div id="footerHandP">
-        <img
-          src="https://cdn-icons-png.flaticon.com/128/1692/1692130.png"
-          alt=""
-        />
-        <p>100% HANDPICKED</p>
-      </div>
-      <div id="assuredQ">
-        <img
-          src="./images/setting.jpg"
-          alt=""
-        />
-        <p>ASSURED QUALITY</p>
-      </div>
+	return `<div id="footerDiv">
+  <div id="firstDivF">
+    <div id="footerBag">
+      <img src="./images/bagIcon.jpeg" alt="" />
+      <!-- https://cdn-icons-png.flaticon.com/128/709/709488.png -->
+      <p>EASY EXCHANGE</p>
     </div>
-    <div id="forbg">
-      <div id="secondDivF">
-        <div>
-          <p>Ajio</p>
-          <a href=""><p>Who We Are</p></a>
-          <a href=""><p>Join Our team</p></a>
-          <a href=""><p>Terms & Conditions</p></a>
-          <a href=""><p>We Respect Your Privacy</p></a>
-          <a href=""><p>Fees & Payments</p></a>
-          <a href=""><p>Returns & Refunds Policy</p></a>
-          <a href=""><p>Promotions Terms & Conditions</p></a>
-        </div>
-        <div>
-          <p>Help</p>
-          <a href=""><p>Track Your Order</p></a>
-          <a href=""><p>Frequently Asked Questions</p></a>
-          <a href=""><p>Returns</p></a>
-          <a href=""><p>Cancellations</p></a>
-          <a href=""><p>Payments</p></a>
-          <a href=""><p>Customer Care</p></a>
-          <a href=""><p>How Do I Redeem my Coupon</p></a>
-        </div>
-        <div>
-          <p>Shop By</p>
-          <a href=""><p>Mens</p></a>
-          <a href=""><p>Womens</p></a>
-          <a href=""><p>Kids</p></a>
-          <a href=""><p>Indie</p></a>
-          <a href=""><p>Stores</p></a>
-          <a href=""><p>New Arrivals</p></a>
-          <a href=""><p>Brand Directory</p></a>
-          <a href=""><p>Home</p></a>
-          <a href=""><p>Collections</p></a>
-        </div>
-        <div>
-          <p>Follow us</p>
-          <a href=""><p>Facebool</p></a>
-          <a href=""><p>Instagram-AJIOlife</p></a>
-          <a href=""><p>Instagram-AJIIO LUXE</p></a>
-          <a href=""><p>Twitter</p></a>
-          <a href=""><p>Pinterest</p></a>
-        </div>
-      </div>
 
-      <div id="thirdDivF"></div>
-      <div id="fourthDivF">
-        <div id="fourthsub1">
-          <p>Payment Methods</p>
-          <p>Secure systems</p>
-        </div>
-        <div id="fourtnsub2">
-          <div><img src="https://assets.ajio.com/static/peassets/public/icons/Net-Banking.svg" alt="" /></div>
-          <div><img src="https://assets.ajio.com/static/peassets/public/icons/visa.svg
-" alt="" /></div>
-          <div><img src="https://assets.ajio.com/static/peassets/public/icons/MasterCard.svg
-" alt="" /></div>
-          <div><img src="https://assets.ajio.com/static/peassets/public/icons/Jio-Money.svg
-" alt="" /></div>
-          <div><img src="https://assets.ajio.com/static/peassets/public/icons/COD.svg
-" alt="" /></div>
+    <div id="footerHandP">
+      <img src="./images/handIcon.jpeg" width="100%" />
+      <!-- https://cdn-icons-png.flaticon.com/128/1692/1692130.png -->
+      <p>100% HANDPICKED</p>
+    </div>
+    <div id="assuredQ">
+      <img src="./images/assured.jpeg" alt="" />
+      <!-- https://cdn-icons.flaticon.com/png/128/3002/premium/3002398.png?token=exp=1642625916~hmac=d030759b7bd5c6d1e822e8c81a61e0d6 -->
+      <p>ASSURED QUALITY</p>
+    </div>
+  </div>
+  <div id="forbg">
+    <div id="secondDivF">
+      <div>
+        <h4>Ajio</h4>
+        <a href=""><p>Who We Are</p></a>
+        <a href=""><p>Join Our team</p></a>
+        <a href=""><p>Terms & Conditions</p></a>
+        <a href=""><p>We Respect Your Privacy</p></a>
+        <a href=""><p>Fees & Payments</p></a>
+        <a href=""><p>Returns & Refunds Policy</p></a>
+        <a href=""><p>Promotions Terms & Conditions</p></a>
+      </div>
+      <div>
+        <h4>Help</h4>
+        <a href=""><p>Track Your Order</p></a>
+        <a href=""><p>Frequently Asked Questions</p></a>
+        <a href=""><p>Returns</p></a>
+        <a href=""><p>Cancellations</p></a>
+        <a href=""><p>Payments</p></a>
+        <a href=""><p>Customer Care</p></a>
+        <a href=""><p>How Do I Redeem my Coupon</p></a>
+      </div>
+      <div>
+        <h4>Shop By</h4>
+        <a href=""><p>Mens</p></a>
+        <a href=""><p>Womens</p></a>
+        <a href=""><p>Kids</p></a>
+        <a href=""><p>Indie</p></a>
+        <a href=""><p>Stores</p></a>
+        <a href=""><p>New Arrivals</p></a>
+      </div>
+      <div>
+        <h4>Follow us</h4>
+        <a href=""><p>Facebook</p></a>
+        <a href=""><p>Instagram-AJIOlife</p></a>
+        <a href=""><p>Instagram-AJIO LUXE</p></a>
+        <a href=""><p>Twitter</p></a>
+        <a href=""><p>Pinterest</p></a>
+      </div>
+    </div>
+
+    <div id="thirdDivF"></div>
+    <div id="fourthDivF">
+      <div id="fourthsub1">
+        <p>Payment Methods</p>
+        <p>Secure systems</p>
+      </div>
+      <div id="fourtnsub2">
+        <div id="payment">
+          <img src="./images/payment.jpeg" alt="" />
         </div>
       </div>
     </div>
-  </div>`;
+  </div>
+</div>`;
 }
 export default footer;
