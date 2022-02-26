60a0b4a5ea0a6352620eb8d2577ba1140fbc80b5
PrafulDeore <91863686+PrafulDeore@users.noreply.github.com>
2022-02-26 11:34:10 +0530

    Pushing Frontend

---
+++ public/footerComponents/footer.js
@@ -0,0 +1,91 @@
+function footer() {
+  return ` <div id="footerDiv">
+    <div id="firstDivF">
+      <div id="footerBag">
+        <img
+          src="https://cdn-icons-png.flaticon.com/128/709/709488.png"
+          alt=""
+        />
+        <p>EASY EXCHANGE</p>
+      </div>
+      <div id="footerHandP">
+        <img
+          src="https://cdn-icons-png.flaticon.com/128/1692/1692130.png"
+          alt=""
+        />
+        <p>100% HANDPICKED</p>
+      </div>
+      <div id="assuredQ">
+        <img
+          src="./images/setting.jpg"
+          alt=""
+        />
+        <p>ASSURED QUALITY</p>
+      </div>
+    </div>
+    <div id="forbg">
+      <div id="secondDivF">
+        <div>
+          <p>Ajio</p>
+          <a href=""><p>Who We Are</p></a>
+          <a href=""><p>Join Our team</p></a>
+          <a href=""><p>Terms & Conditions</p></a>
+          <a href=""><p>We Respect Your Privacy</p></a>
+          <a href=""><p>Fees & Payments</p></a>
+          <a href=""><p>Returns & Refunds Policy</p></a>
+          <a href=""><p>Promotions Terms & Conditions</p></a>
+        </div>
+        <div>
+          <p>Help</p>
+          <a href=""><p>Track Your Order</p></a>
+          <a href=""><p>Frequently Asked Questions</p></a>
+          <a href=""><p>Returns</p></a>
+          <a href=""><p>Cancellations</p></a>
+          <a href=""><p>Payments</p></a>
+          <a href=""><p>Customer Care</p></a>
+          <a href=""><p>How Do I Redeem my Coupon</p></a>
+        </div>
+        <div>
+          <p>Shop By</p>
+          <a href=""><p>Mens</p></a>
+          <a href=""><p>Womens</p></a>
+          <a href=""><p>Kids</p></a>
+          <a href=""><p>Indie</p></a>
+          <a href=""><p>Stores</p></a>
+          <a href=""><p>New Arrivals</p></a>
+          <a href=""><p>Brand Directory</p></a>
+          <a href=""><p>Home</p></a>
+          <a href=""><p>Collections</p></a>
+        </div>
+        <div>
+          <p>Follow us</p>
+          <a href=""><p>Facebool</p></a>
+          <a href=""><p>Instagram-AJIOlife</p></a>
+          <a href=""><p>Instagram-AJIIO LUXE</p></a>
+          <a href=""><p>Twitter</p></a>
+          <a href=""><p>Pinterest</p></a>
+        </div>
+      </div>
+
+      <div id="thirdDivF"></div>
+      <div id="fourthDivF">
+        <div id="fourthsub1">
+          <p>Payment Methods</p>
+          <p>Secure systems</p>
+        </div>
+        <div id="fourtnsub2">
+          <div><img src="https://assets.ajio.com/static/peassets/public/icons/Net-Banking.svg" alt="" /></div>
+          <div><img src="https://assets.ajio.com/static/peassets/public/icons/visa.svg
+" alt="" /></div>
+          <div><img src="https://assets.ajio.com/static/peassets/public/icons/MasterCard.svg
+" alt="" /></div>
+          <div><img src="https://assets.ajio.com/static/peassets/public/icons/Jio-Money.svg
+" alt="" /></div>
+          <div><img src="https://assets.ajio.com/static/peassets/public/icons/COD.svg
+" alt="" /></div>
+        </div>
+      </div>
+    </div>
+  </div>`;
+}
+export default footer;
